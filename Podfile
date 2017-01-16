ad149452abdd6dd5dc636bc0c8ee0fe4eacff67b
George <george@immedia.co.za>
2017-01-16 14:58:25 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+ platform :ios, '9.0'
+
+target 'ExampleCore' do
+
+  use_frameworks!
+
+pod 'Alamofire', '~> 4.0'
+end
---
+++ Pods/Target Support Files/Pods-ExampleCore/Pods-ExampleCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExampleCore : NSObject
+@end
+@implementation PodsDummy_Pods_ExampleCore
+@end
